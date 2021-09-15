a03f89c5829207d5e912268bbfb1aa98864ed22a
Snake <chaldrich24@gmail.com>
2021-09-14 23:17:15 -0400

    add answer validation with new function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,57 @@ var startButtonEl = document.querySelector("#begin-btn");
 var mainEl = document.querySelector("#quiz-content");
 var questions = [
     {question: "What",
-    answers: ["dog","cat"]
+    answers: ["dog","cat"],
+    result: ["correct","wrong"]
     },
-    {question: "Where",
-    wrongOne: "Dog",
-    wrongTwo: "Cat",
-    wrongThree: "Duck",
-    correct: "Bat"
-    },
-    {question: "Where",
-    wrongOne: "Dog",
-    wrongTwo: "Cat",
-    wrongThree: "Duck",
-    correct: "Bat"
+    {question: "Who",
+    answers: ["dog","cat"],
+    result: ["correct","wrong"]
     }
 ];
 var questionCounter = 0;
-console.dir(questions);
 
-
-var startQuiz = function() {
+var askQuestion = function() {
     mainEl.innerHTML = "";
+    var questionEl = document.createElement("div");
+    questionEl.innerHTML = "<h1 class='main-title'>" + questions[questionCounter].question + "</h1>";
+    questionEl.appendChild(generateAnswers());
+    mainEl.appendChild(questionEl);
+    document.querySelector(".answers-container").addEventListener("click", displayResult);
+};
+
+var generateAnswers = function() {
+    var answerContEl = document.createElement("div");
+    answerContEl.className = "answers-container";
+    var currentQuestion = questions[questionCounter];
+    for (i = 0; i < currentQuestion.answers.length; i++) {
+        var answerButtonEl = document.createElement("button");
+        answerButtonEl.type = "button";
+        answerButtonEl.className = "answer-btn";
+        if (currentQuestion.result[i] === "correct") {
+            answerButtonEl.setAttribute("value","correct");
+        }
+        else {
+            answerButtonEl.setAttribute("value", "incorrect");
+        }
+        answerButtonEl.innerHTML = currentQuestion.answers[i];
+        answerContEl.appendChild(answerButtonEl);
+    }
+    return answerContEl;
+};
+
+var displayResult = function(event) {
+    selectedAnswer = event.target.value;
+    document.querySelector(".answers-container").removeEventListener("click", displayResult);
+    if (selectedAnswer === "correct") {
+        alert("Correct!");
+    }
+    else {
+        alert("Incorrect.")
+    }
+    questionCounter++;
     askQuestion();
 };
 
-var askQuestion = function() {
-    var questionEl = document.createElement("div");
-    questionEl.innerHTML = "<h1 class='main-title'>" + questions[questionCounter].question + "</h1><button type='button'>" + questions[questionCounter].answers[0] + "</button>";
-    mainEl.appendChild(questionEl);
-};
+startButtonEl.addEventListener("click", askQuestion);
 
-startButtonEl.addEventListener("click", startQuiz);
